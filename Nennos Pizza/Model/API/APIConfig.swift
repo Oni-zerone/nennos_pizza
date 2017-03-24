81df9b4b6d5909a6e5af1b14338b5a2be518a574
Andrea Altea <oni.zerone@gmail.com>
2017-03-24 19:44:05 +0100

    Feat: define API Config

---
+++ Nennos Pizza/Model/API/APIConfig.swift
@@ -0,0 +1,18 @@
+//
+//  APIConfig.swift
+//  Nennos_Pizza
+//
+//  Created by Andrea Altea on 24/03/17.
+//  Copyright © 2017 StudiOUT. All rights reserved.
+//
+
+import Foundation
+
+struct APIConfig {
+    
+    //MARK: URL parameters
+    
+    public static var scheme: String!
+    public static var host: String!
+    
+}
