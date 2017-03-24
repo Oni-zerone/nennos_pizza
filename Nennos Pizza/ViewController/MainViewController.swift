d82908f7fd3036ed1485d1f3fc748ae2f0036e5f
Andrea Altea <oni.zerone@gmail.com>
2017-03-24 23:54:46 +0100

    Feat: declare tableViewDataSource

--- Nennos Pizza/ViewController/MainViewController.swift
+++ Nennos Pizza/ViewController/MainViewController.swift
@@ -10,17 +10,45 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    //Model
+    fileprivate var pizzas: Array<Pizza> = [] {
+        
+        didSet {
+            self.tableView.reloadData()
+        }
+    }
+    
+    //Views
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        Model.shared.getPizzas { (pizzas) in
+            self.pizzas = pizzas
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+}
+
+extension MainViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.pizzas.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        return UITableViewCell()
     }
     
 }
