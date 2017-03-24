e2157f902dcff642db3852262d0164aa886b987d
Andrea Altea <oni.zerone@gmail.com>
2017-03-24 23:43:19 +0100

    Feat: declare Main ViewController

--- Nennos Pizza/ViewController/MainViewController.swift
+++ Nennos Pizza/ViewController/MainViewController.swift
@@ -1,6 +1,6 @@
 //
-//  ViewController.swift
-//  Nennos Pizza
+//  MainViewController.swift
+//  Nennos_Pizza
 //
 //  Created by Andrea Altea on 24/03/17.
 //  Copyright © 2017 StudiOUT. All rights reserved.
@@ -8,18 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
-
