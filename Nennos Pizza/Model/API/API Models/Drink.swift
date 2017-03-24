c319f7eb1a226d4a44b9f283de043fdc285c0672
Andrea Altea <oni.zerone@gmail.com>
2017-03-24 20:51:40 +0100

    Feat: add Drink model

---
+++ Nennos Pizza/Model/API/API Models/Drink.swift
@@ -0,0 +1,30 @@
+//
+//  Drink.swift
+//  Nennos_Pizza
+//
+//  Created by Andrea Altea on 24/03/17.
+//  Copyright © 2017 StudiOUT. All rights reserved.
+//
+
+import Foundation
+
+struct Drink {
+    
+    let id: Double
+    let name: String
+    let price: Double
+    
+     init?(resource: Dictionary<String, Any>) {
+        
+        guard let id = resource["id"] as? Double,
+            let name = resource["name"] as? String,
+            let price = resource["price"] as? Double else {
+            
+                return nil
+            }
+        
+        self.id = id
+        self.name = name
+        self.price = price
+    }
+}
