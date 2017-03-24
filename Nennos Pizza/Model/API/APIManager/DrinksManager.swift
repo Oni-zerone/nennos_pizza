94d8d5a606d4f65f7bbb3eb3ecde2c41255fe8bb
Andrea Altea <oni.zerone@gmail.com>
2017-03-24 21:45:33 +0100

    Feat: add Drinks API

---
+++ Nennos Pizza/Model/API/APIManager/DrinksManager.swift
@@ -0,0 +1,43 @@
+//
+//  DrinksManager.swift
+//  Nennos_Pizza
+//
+//  Created by Andrea Altea on 24/03/17.
+//  Copyright © 2017 StudiOUT. All rights reserved.
+//
+
+import Foundation
+
+fileprivate typealias DrinksManager = APIManager
+
+extension DrinksManager {
+
+    static func getIngredients(session: URLSession = APIConfig.session, completion: @escaping(Array<Drink>?, Error?) -> ()) {
+        
+        guard let URL = APIManager.URLForResource(resourcePath: "N1mnOA_oz") else {
+            
+            return completion(nil, NSError.invalidPath())
+        }
+        
+        let task = session.dataTask(with: APIRequest(url: URL) as URLRequest, completionHandler: APIManager.responseArrayCheck({ (response, error) in
+            
+            if let e = error {
+                
+                return completion(nil, e)
+            }
+            
+            guard let items = response as? Array<Dictionary<String, Any>> else {
+                
+                return completion(nil, NSError.invalidContent())
+            }
+            
+            let ingredients = items.flatMap({ (item) -> Drink? in
+                return Drink(resource: item)
+            })
+            
+            completion(ingredients, nil)
+        }))
+        
+        task.resume()
+    }
+}
