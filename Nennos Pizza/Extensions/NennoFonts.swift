f3f55f6f9d13358594186e5d15f1afd11368359c
Andrea Altea <oni.zerone@gmail.com>
2017-03-25 15:22:39 +0100

    Feat: define Nenno's Pizza fonts

---
+++ Nennos Pizza/Extensions/NennoFonts.swift
@@ -0,0 +1,17 @@
+//
+//  NennoFonts.swift
+//  Nennos_Pizza
+//
+//  Created by Andrea Altea on 25/03/17.
+//  Copyright © 2017 StudiOUT. All rights reserved.
+//
+
+import UIKit
+
+extension UIFont {
+    
+    static let titleFont = UIFont(name: "SFUIText-Bold", size: 24)
+    static let textFont = UIFont(name: "SFUIText-Regular", size: 14)
+    
+    static let buttonFont = UIFont(name: "SFUIText-Bold", size: 16)
+}
