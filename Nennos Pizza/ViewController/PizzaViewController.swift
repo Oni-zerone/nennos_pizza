f763b2c2bcf71f7d39394b5231d1b428555cc57e
Andrea Altea <oni.zerone@gmail.com>
2017-03-25 19:01:49 +0100

    Feat: add showPizza segue

---
+++ Nennos Pizza/ViewController/PizzaViewController.swift
@@ -0,0 +1,36 @@
+//
+//  PizzaViewController.swift
+//  Nennos_Pizza
+//
+//  Created by Andrea Altea on 25/03/17.
+//  Copyright © 2017 StudiOUT. All rights reserved.
+//
+
+import UIKit
+
+class PizzaViewController: UIViewController {
+
+    //Model
+    var pizza: Pizza!
+    
+    @IBOutlet weak var pizzaLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        self.pizzaLabel.text = pizza.nam
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+}
