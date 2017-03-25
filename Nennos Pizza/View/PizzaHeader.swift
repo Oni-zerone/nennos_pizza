f8c2ed18593b9ac81c602ae0eb5baec43c2e605a
Andrea Altea <oni.zerone@gmail.com>
2017-03-25 22:10:02 +0100

    Feat: define pizzaHeader

---
+++ Nennos Pizza/View/PizzaHeader.swift
@@ -0,0 +1,25 @@
+//
+//  PizzaHeader.swift
+//  Nennos_Pizza
+//
+//  Created by Andrea Altea on 25/03/17.
+//  Copyright © 2017 StudiOUT. All rights reserved.
+//
+
+import UIKit
+
+class PizzaHeader: UIView {
+
+    init(frame: CGRect) {
+        
+        super.init(frame: frame)
+    }
+    
+    init?(coder aDecoder: NSCoder) {
+        
+        super.init(coder: aDecoder)
+    }
+    
+    func setupViews()
+
+}
