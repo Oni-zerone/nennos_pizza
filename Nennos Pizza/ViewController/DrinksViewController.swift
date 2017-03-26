2e963ba9d65562ad3c961f5ccc45b0ac4995a550
Andrea Altea <oni.zerone@gmail.com>
2017-03-27 01:22:37 +0200

    Feat: show Drinks in DrinksViewController

--- Nennos Pizza/ViewController/DrinksViewController.swift
+++ Nennos Pizza/ViewController/DrinksViewController.swift
@@ -10,15 +10,38 @@ import UIKit
 
 class DrinksViewController: UIViewController {
 
+    //Model
+    var dataSource: PricedItemDataSource<Drink>!
+    
+    //View
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.title = "DRINKS"
+        
+        self.setupTableView()
+        
+        Model.shared.getDrinks { (drinks) in
+            self.dataSource.items = drinks
+        }
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    func setupTableView() {
+        
+        self.tableView.estimatedRowHeight = 44
+        let cellName = String(describing: DrinkTableViewCell.self)
+        self.tableView.register(DrinkTableViewCell.self, forCellReuseIdentifier: cellName)
+        
+        self.dataSource = PricedItemDataSource<Drink>(with: cellName, tableView: self.tableView)
+    }
 }
