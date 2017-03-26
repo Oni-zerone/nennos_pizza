46f60a04af1166950f68ba071e1e7ff4c59984b1
Andrea Altea <oni.zerone@gmail.com>
2017-03-26 22:23:01 +0200

    Feat: declare CartDatasource

---
+++ Nennos Pizza/ViewController/DataSource/CartDatasource.swift
@@ -0,0 +1,15 @@
+//
+//  CartDatasource.swift
+//  Nennos_Pizza
+//
+//  Created by Andrea Altea on 26/03/17.
+//  Copyright © 2017 StudiOUT. All rights reserved.
+//
+
+import UIKit
+
+class CartDatasource: BaseDataSource<ShippableItem> {
+
+    
+    
+}
