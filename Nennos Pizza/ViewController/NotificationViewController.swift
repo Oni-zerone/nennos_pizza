482658183e31fd1950482dbab28ecf99cbf020ae
Andrea Altea <oni.zerone@gmail.com>
2017-03-26 19:07:30 +0200

    Feat: register NotificaitonViewController to the added to cart Notification

--- Nennos Pizza/ViewController/NotificationViewController.swift
+++ Nennos Pizza/ViewController/NotificationViewController.swift
@@ -16,9 +16,28 @@ class NotificationViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(didAddPizza(with:)),
+                                               name: Cart.Notifications.didAddPizza,
+                                               object: nil)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        NotificationCenter.default.removeObserver(self,
+                                                  name: Cart.Notifications.didAddPizza,
+                                                  object: nil)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    func didAddPizza(with notification: Notification) {
+        print("added pizza to cart")
+    }
 }
