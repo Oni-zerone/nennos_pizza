2618ecd400b1a83b8a1b82220738f243810c36ca
Andrea Altea <oni.zerone@gmail.com>
2017-03-26 22:53:05 +0200

    Feat: base implementation of tableView

--- Nennos Pizza/ViewController/CartViewController.swift
+++ Nennos Pizza/ViewController/CartViewController.swift
@@ -10,18 +10,38 @@ import UIKit
 
 class CartViewController: UIViewController {
 
+    //Model
+    var dataSource: CartDatasource!
+    
+    //View
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var checkoutControl: PriceControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.title = "CART"
+        
+        self.setupTableView()
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.dataSource.items = Model.shared.cart.items
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func setupTableView() {
+        
+        self.tableView.estimatedRowHeight = 178
+        let cellName = String(describing: MainTableViewCell.self)
+        
+        self.tableView.register(UINib(nibName: cellName , bundle: Bundle.main), forCellReuseIdentifier: cellName)
+        self.dataSource = CartDatasource(with: cellName, tableView: self.tableView)
     }
     
     @IBAction func checkoutAction(_ sender: Any) {
