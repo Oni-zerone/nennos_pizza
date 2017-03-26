29bf465b5f9da879c7f4d7797c46f2b83cbb79d5
Andrea Altea <oni.zerone@gmail.com>
2017-03-27 00:40:34 +0200

    Feat: declare DrinksViewController

---
+++ Nennos Pizza/ViewController/DrinksViewController.swift
@@ -0,0 +1,24 @@
+//
+//  DrinksViewController.swift
+//  Nennos_Pizza
+//
+//  Created by Andrea Altea on 27/03/17.
+//  Copyright © 2017 StudiOUT. All rights reserved.
+//
+
+import UIKit
+
+class DrinksViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
