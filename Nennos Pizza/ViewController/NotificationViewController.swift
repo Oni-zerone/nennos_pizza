8e3939016bd1bd42c4522c45d315b14d00cc2a68
Andrea Altea <oni.zerone@gmail.com>
2017-03-26 18:49:18 +0200

    Feat: declare NotificationViewController

---
+++ Nennos Pizza/ViewController/NotificationViewController.swift
@@ -0,0 +1,24 @@
+//
+//  NotificationViewController.swift
+//  Nennos_Pizza
+//
+//  Created by Andrea Altea on 26/03/17.
+//  Copyright © 2017 StudiOUT. All rights reserved.
+//
+
+import UIKit
+
+class NotificationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
