5f1d8d3f17db9ba1169f60a6432b32e4088ea476
Andrea Altea <oni.zerone@gmail.com>
2017-03-26 19:38:13 +0200

    Feat: add notification view

--- Nennos Pizza/ViewController/NotificationViewController.swift
+++ Nennos Pizza/ViewController/NotificationViewController.swift
@@ -37,7 +37,68 @@ class NotificationViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
     func didAddPizza(with notification: Notification) {
-        print("added pizza to cart")
+        
+        let notificationView = self.prepareNotificationView()
+        self.view.addSubview(notificationView)
+        //Append
+        let hConstraints = NSLayoutConstraint.constraints(withVisualFormat: "|-0-[view]-0-|",
+                                                          options: NSLayoutFormatOptions(rawValue: 0),
+                                                          metrics: nil,
+                                                          views: ["view" : notificationView]);
+        let vConstraint = NSLayoutConstraint(item: notificationView,
+                                             attribute: .bottom,
+                                             relatedBy: .equal,
+                                             toItem: self.view,
+                                             attribute: .top,
+                                             multiplier: 1,
+                                             constant: 0)
+        
+        self.view.addConstraints(hConstraints);
+        self.view.addConstraint(vConstraint);
+        
+        self.animateNotificaitonView(notificationView)
+    }
+    
+    private func prepareNotificationView() -> UIView {
+        
+        let label = UILabel()
+        label.backgroundColor = UIColor.primaryColor
+        label.textColor = UIColor.white
+        label.font = UIFont.notificationFont
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.text = "ADDED TO CART"
+        
+        let heightConstraint = NSLayoutConstraint(item: label,
+                                                  attribute: .height,
+                                                  relatedBy: .equal,
+                                                  toItem: nil,
+                                                  attribute: .notAnAttribute,
+                                                  multiplier: 1,
+                                                  constant: 20)
+        label.addConstraint(heightConstraint)
+        return label
+    }
+    
+    func animateNotificaitonView(_ notificationView: UIView) {
+        
+        self.view.layoutIfNeeded()
+        
+        UIView.animateKeyframes(withDuration: 3.2, delay: 0, options: .allowUserInteraction, animations: {
+            
+            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.1, animations: {
+                
+                notificationView.transform = CGAffineTransform(translationX: 0, y: notificationView.frame.height)
+            })
+            
+            UIView.addKeyframe(withRelativeStartTime: 0.96, relativeDuration: 0.1, animations: {
+                notificationView.transform = CGAffineTransform.identity
+            })
+            
+        }) { (finished) in
+            notificationView.removeFromSuperview()
+        }
     }
 }
