2e99d8e7ba940c5be7bef6b3939396b64b77e4a1
Andrea Altea <oni.zerone@gmail.com>
2017-03-27 19:03:44 +0200

    Declare CheckoutManager

---
+++ Nennos Pizza/Model/Checkout/CheckoutManager.swift
@@ -0,0 +1,16 @@
+//
+//  CheckoutManager.swift
+//  Nennos_Pizza
+//
+//  Created by Andrea Altea on 27/03/17.
+//  Copyright © 2017 StudiOUT. All rights reserved.
+//
+
+import Foundation
+
+struct CheckoutManager {
+    
+    static func send(_ cart: Cart, success:(Bool, Error) -> ()) {
+        
+    }
+}
